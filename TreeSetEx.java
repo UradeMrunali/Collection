f0df5be3ea4a47ad287d5df7397ad24eb92245e7
Mrunali <mrunaliurade26@gmail.com>
2023-06-18 18:54:10 +0530

    example of Collection

---
+++ TreeSetEx.java
@@ -0,0 +1,18 @@
+import java.util.TreeSet;
+
+public class TreeSetEx {
+ public static void main(String[] args) {
+	
+	 TreeSet<String> t =new TreeSet<String>();
+	 
+	 t.add("ears");
+	 t.add("banana");
+	 t.add("apple");
+	 t.add("dog");
+	 t.add("chikoo");
+	 t.add("apple");
+	// t.add(null);
+	 
+	 System.out.println(t);
+}
+}
