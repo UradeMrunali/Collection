b753ec6a79081a61100722a70a745fa9d29dd6f9
Mrunali <mrunaliurade26@gmail.com>
2023-06-19 11:20:14 +0530

    example of Collection

---
+++ VecCls.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class VecCls {
+  public static void main(String[] args) {
+	
+	 Vector a=new Vector(5,3);
+		
+		a.addElement(50);
+		a.addElement("by");
+		a.addElement('h');
+		a.addElement(2.5f);
+		a.addElement(50.27);
+		a.addElement('h');
+		a.addElement(67);
+		a.addElement("hiiii");
+		a.addElement('h');
+		
+		System.out.println(a.size());
+		System.out.println(a.capacity());
+	
+}
+}
